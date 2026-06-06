427690d97ec439b45b1fc369d40c5d4954ddbf3e
JIHYUNG KIM <martinjhkim7493@gmail.com>
2026-06-06 23:42:45 +0900

    feat: ResponseEntity 추가

--- src/main/java/com/example/spring_rest_api/user/controller/UserController.java
+++ src/main/java/com/example/spring_rest_api/user/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.example.spring_rest_api.user.controller;
 
+import com.example.spring_rest_api.common.response.ApiResponse;
 import com.example.spring_rest_api.user.service.UserService;
 import com.example.spring_rest_api.user.service.request.UserCreateRequest;
 import com.example.spring_rest_api.user.service.request.UserUpdateRequest;
 import com.example.spring_rest_api.user.service.response.UserResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,27 +17,43 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users")
-    public UserResponse create(@Valid @RequestBody UserCreateRequest request) {
-        return userService.create(request);
+    public ResponseEntity<ApiResponse<UserResponse>> create(@Valid @RequestBody UserCreateRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.of(
+                        "register_success",
+                        userService.create(request)
+                ));
     }
 
     @GetMapping("/users/{userId}")
-    public UserResponse read(@PathVariable Long userId) {
-        return userService.read(userId);
+    public ResponseEntity<ApiResponse<UserResponse>> read(@PathVariable Long userId) {
+        return ResponseEntity.ok(ApiResponse.of(
+                "get_user_info_success",
+                userService.read(userId)
+        ));
     }
 
     @PatchMapping("/users/{userId}")
-    public UserResponse updateInformation(@PathVariable Long userId, @Valid @RequestBody UserUpdateRequest request) {
-        return userService.updateInformation(userId, request);
+    public ResponseEntity<ApiResponse<UserResponse>> updateInformation(@PathVariable Long userId, @Valid @RequestBody UserUpdateRequest request) {
+        return ResponseEntity.ok(ApiResponse.of(
+                "user_info_update_success",
+                userService.updateInformation(userId, request)
+        ));
     }
 
     @PatchMapping("/users/{userId}/password")
-    public UserResponse updatePassword(@PathVariable Long userId, @Valid @RequestBody UserUpdateRequest request) {
-        return userService.updatePassword(userId, request);
+    public ResponseEntity<ApiResponse<UserResponse>> updatePassword(@PathVariable Long userId, @Valid @RequestBody UserUpdateRequest request) {
+        return ResponseEntity.ok(ApiResponse.of(
+                "user_password_update_success",
+                userService.updatePassword(userId, request)
+        ));
     }
 
     @DeleteMapping("/users/{userId}")
-    public UserResponse delete(@PathVariable Long userId) {
-        return userService.delete(userId);
+    public ResponseEntity<ApiResponse<UserResponse>> delete(@PathVariable Long userId) {
+        return ResponseEntity.ok(ApiResponse.of(
+                "user_delete_success",
+                userService.delete(userId)
+        ));
     }
 }
