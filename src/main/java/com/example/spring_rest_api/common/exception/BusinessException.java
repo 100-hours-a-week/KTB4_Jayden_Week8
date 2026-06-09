c38ba878e77ca07bd732b846c5e06c75ad922e25
JIHYUNG KIM <martinjhkim7493@gmail.com>
2026-06-10 00:10:55 +0900

    fix: BusinessException 응답 출력 수정

--- src/main/java/com/example/spring_rest_api/common/exception/BusinessException.java
+++ src/main/java/com/example/spring_rest_api/common/exception/BusinessException.java
@@ -1,7 +1,11 @@
 package com.example.spring_rest_api.common.exception;
 
+import lombok.Getter;
+import lombok.ToString;
 import org.springframework.http.HttpStatus;
 
+@Getter
+@ToString
 public class BusinessException extends RuntimeException {
     private final String message;
     private final HttpStatus status;
