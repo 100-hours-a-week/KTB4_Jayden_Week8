484c462e9cd443c65a0e0f773f7bcd4dae401b8f
JIHYUNG KIM <martinjhkim7493@gmail.com>
2026-06-11 14:27:34 +0900

    fix: Exception 반환 수정

--- src/main/java/com/example/spring_rest_api/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/spring_rest_api/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.example.spring_rest_api.common.exception;
 
-import com.example.spring_rest_api.common.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,14 +9,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ApiResponse<Void>> handleBusiness(BusinessException exception) {
+    public ResponseEntity<ErrorResponseDto> handleBusiness(BusinessException exception) {
         return ResponseEntity.status(exception.getStatus())
-                .body(ApiResponse.of(exception.getMessage(), null));
+                .body(ErrorResponseDto.of(exception.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<Void>> handleNotValid(MethodArgumentNotValidException exception) {
+    public ResponseEntity<ErrorResponseDto> handleNotValid(MethodArgumentNotValidException exception) {
         return ResponseEntity.status(exception.getStatusCode())
-                .body(ApiResponse.of(exception.getFieldError().getDefaultMessage(), null));
+                .body(ErrorResponseDto.of(exception.getFieldError().getDefaultMessage()));
     }
 }
