8edf7d952b45332efc7c5398ed8c9f079590a42a
JIHYUNG KIM <martinjhkim7493@gmail.com>
2026-06-25 16:34:48 +0900

    feat: Exception, HttpMessageNotReadableException

--- src/main/java/com/example/spring_rest_api/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/spring_rest_api/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.example.spring_rest_api.common.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Optional;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,6 +21,19 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponseDto> handleNotValid(MethodArgumentNotValidException exception) {
         return ResponseEntity.status(exception.getStatusCode())
-                .body(ErrorResponseDto.of(exception.getFieldError().getDefaultMessage()));
+                .body(ErrorResponseDto.of(Optional.ofNullable(exception.getFieldError())
+                        .orElseThrow().getDefaultMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleException(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorResponseDto.of(exception.getMessage()));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponseDto> handleException(HttpMessageNotReadableException exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(ErrorResponseDto.of(exception.getMessage()));
     }
 }
