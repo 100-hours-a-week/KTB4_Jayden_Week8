e8cee9b9defcddb4c28bde2ea5369ef64960f8d6
JIHYUNG KIM <martinjhkim7493@gmail.com>
2026-06-30 15:40:34 +0900

    fix: login service

--- src/main/java/com/example/spring_rest_api/authorization/service/AuthService.java
+++ src/main/java/com/example/spring_rest_api/authorization/service/AuthService.java
@@ -4,6 +4,8 @@ import com.example.spring_rest_api.authorization.service.request.AuthRequest;
 import com.example.spring_rest_api.authorization.service.response.AuthResponse;
 import com.example.spring_rest_api.common.exception.ForbiddenException;
 import com.example.spring_rest_api.common.exception.UnauthorizedException;
+import com.example.spring_rest_api.user.entity.User;
+import com.example.spring_rest_api.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,11 +14,16 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+    private final UserRepository userRepository;
 
     public AuthResponse login(AuthRequest request) {
         log.info("Login request: {}", request.getEmail());
 
-        if (request.getEmail().equals("abc@abc.com") && request.getPassword().equals("abc1234!")) {
+        String email = request.getEmail();
+        String password = request.getPassword();
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UnauthorizedException("INVALID_EMAIL"));
+
+        if (user.getPassword().equals(password)) {
             return AuthResponse.from("abc1234567890");
         } else {
             throw new UnauthorizedException("UNAUTHORIZED");
